c49e5039792ef6ae40e22a165c93f0fe4f434e6b
Masha <tok.masha.dm@yandex.ru>
2023-07-30 16:46:32 +0700

    meow

---
+++ app/src/main/java/com/example/notificationgrep/NotificationListenerExampleService.kt
@@ -0,0 +1,83 @@
+package com.example.notificationgrep
+
+import android.content.Intent
+import android.os.IBinder
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016 Fábio Alves Martins Pereira (Chagall)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+class NotificationListenerExampleService : NotificationListenerService() {
+    /*
+        These are the package names of the apps. for which we want to
+        listen the notifications
+     */
+    private object ApplicationPackageNames {
+        const val FACEBOOK_PACK_NAME = "com.facebook.katana"
+        const val FACEBOOK_MESSENGER_PACK_NAME = "com.facebook.orca"
+        const val WHATSAPP_PACK_NAME = "com.whatsapp"
+        const val INSTAGRAM_PACK_NAME = "com.instagram.android"
+    }
+
+    /*
+        These are the return codes we use in the method which intercepts
+        the notifications, to decide whether we should do something or not
+     */
+    object InterceptedNotificationCode {
+        const val NOTIFICATION_CODE = 4 // We ignore all notification with code == 4
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return super.onBind(intent)
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        Log.w("asd", "got notification " + sbn.packageName + " " + sbn.notification.extras.getCharSequence("android.text").toString())
+        val notificationCode = matchNotificationCode(sbn)
+        if (true) {
+            val intent = Intent("com.github.chagall.notificationlistenerexample")
+            intent.putExtra("Notification Code", notificationCode)
+            sendBroadcast(intent)
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        val notificationCode = matchNotificationCode(sbn)
+        if (true) {
+            val activeNotifications = this.activeNotifications
+            if (activeNotifications != null && activeNotifications.size > 0) {
+                for (i in activeNotifications.indices) {
+                    if (notificationCode == matchNotificationCode(activeNotifications[i])) {
+                        val intent = Intent("com.example.notifiationgrep")
+                        intent.putExtra("Notification Code", notificationCode)
+                        sendBroadcast(intent)
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    private fun matchNotificationCode(sbn: StatusBarNotification): Int {
+        val packageName = sbn.packageName
+        return InterceptedNotificationCode.NOTIFICATION_CODE
+    }
+}
